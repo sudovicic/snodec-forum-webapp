49aa170a281369762e7bc8eee89e9ffa04d273e6
la-lisa <lamplmair.lisa@gmx.at>
2022-09-26 20:15:28 +0200

    add working CMake and user table creation

--- main.cpp
+++ main.cpp
@@ -6,37 +6,70 @@
 #include "database/mariadb/MariaDBClient.h"
 #include "database/mariadb/MariaDBCommandSequence.h"
 
-#define SERVERROOT "/home/student/Documents/Code/projects/forum-webapp/public"
+#define SERVERROOT "/home/student/forum-webapp/public"
 #define PORT 1337
 
 int main(int argc, char *argv[]) {
     express::legacy::in::WebApp::init(argc, argv);
 
-    database::mariadb::MariaDBConnectionDetails details = {
-        .hostname = "localhost",
-        .username = "snodec",
-        .password = "pentium5",
-        .database = "snodec",
-        .port = 3306,
-        .socket = "/run/mysqld/mysqld.sock",
-        .flags = 0,
+    const database::mariadb::MariaDBConnectionDetails details = {
+        "localhost",
+        "snodec",
+        "password",
+        "WebForumDB",
+        3307,
+        "/var/run/mysqld/mysqld.sock",
+        0,
     };
 
-    database::mariadb::MariaDBClient db1(details);
+    database::mariadb::MariaDBClient mariaDbClient(details);
+
+    std::string sqlString = "Select * from User;";
+
+    mariaDbClient.query(sqlString, [&](const MYSQL_ROW &rows) {
+           if (rows != nullptr) {
+               for (int i = 0; rows[i + 1]; ++i) {
+                   std::cout << rows[i] << std::endl;
+
+               }
+           }
+
+       }, [](const std::string &, int) {
+       })
+
+    .exec(
+                    "CREATE TABLE IF NOT EXISTS `Users` (name varchar(255), password varchar(255))",
+                    [&mariaDbClient](void) -> void {
+            VLOG(0) << "**** OnQuery 0;";
+            mariaDbClient.affectedRows(
+                        [](my_ulonglong affectedRows) -> void {
+                VLOG(0) << "**** AffectedRows 1: " << affectedRows;
+            },
+            [](const std::string& errorString, unsigned int errorNumber) -> void {
+                VLOG(0) << "Error 1: " << errorString << " : " << errorNumber;
+            });
+        },
+        [](const std::string& errorString, unsigned int errorNumber) -> void {
+            VLOG(0) << "**** Error 0: " << errorString << " : " << errorNumber;
+        });
+
 
     express::legacy::in::WebApp staticServer("ForumApp");
 
-    staticServer.use(express::middleware::StaticMiddleware(SERVERROOT));
+        staticServer.use(express::middleware::StaticMiddleware(SERVERROOT));
 
-    staticServer.listen(PORT, [] (const express::legacy::in::WebApp::SocketAddress &socketAddress, int err) -> void {
-        if (err != 0) {
-            std::cerr << "Listen failed on port " << PORT << std:: endl;
-        } else {
-            std::cout << "ForumApp is listening on " << socketAddress.toString() << std::endl;
-        }
-    });
+        staticServer.listen(8080, [] (const express::legacy::in::WebApp::SocketAddress &socketAddress, int err) -> void {
+            if (err != 0) {
+                std::cerr << "Listen failed on port 8080 " << std:: endl;
+            } else {
+                std::cout << "ForumApp is listening on " << socketAddress.toString() << std::endl;
+            }
+        });
 
-//    express::legacy::in::WebApp restAPI("Forum Rest API");
+    express::legacy::in::WebApp restAPI("ForumRestAPI");
+
+
+
 
 //    restAPI.get(
 //        "/api/subtopics",
@@ -59,3 +92,4 @@ int main(int argc, char *argv[]) {
 
     return express::WebApp::start();
 }
+
